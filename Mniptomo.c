9890b0e5887257c2a7c57be64b52ce7cf8618eab
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-12-10 17:50:39 -0300

    Modify sfniptomo to return rays end points #14
    Organize the program and include a for loop to several rays. The
    purpose is to store rays end points and angles (It will be the NIP
    sources in NIP tomography inversion).

--- Mniptomo.c
+++ Mniptomo.c
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
 	t0s = sf_input("t0s");
 	angles = sf_input("anglefile");
 
-	/* get 2D grid parameters */
+	/* Velocity model: get 2D grid parameters */
 	if(!sf_histint(vel,"n1",n)) sf_error("No n1= in input");
 	if(!sf_histint(vel,"n2",n+1)) sf_error("No n2= in input");
 	if(!sf_histfloat(vel,"d1",d)) sf_error("No d1= in input");
@@ -53,27 +53,23 @@ int main(int argc, char* argv[])
 	if(!sf_getbool("verb",&verb)) verb=true;
 	/* verbose */
 
-	if(verb){
-		sf_warning("Verbose on");
-	}
-
+	/* Shotsfile: get m0 shot points */
 	if(!sf_histint(shots,"n1",&ndim) || 2 != ndim)
 		sf_error("Must have n1=2 in shotsfile");
 	if(!sf_histint(shots,"n2",&nshot)) sf_error("No n2= in shotfile");
 	if(sf_histfloat(shots,"o2",&t)) sf_putfloat(rays,"o3",t);
 	if(sf_histfloat(shots,"d2",&t)) sf_putfloat(rays,"d3",t);
-
 	s = sf_floatalloc2(ndim,nshot);
 	sf_floatread(s[0],ndim*nshot,shots);
 	sf_fileclose(shots);
 
+	/* Anglefile: get initial emergence angle */
 	if(!sf_histint(angles,"n1",&nr)) sf_error("No n1= in anglefile");
-
 	a = sf_floatalloc(nr);
 	sf_floatread(a,nr,angles);
 	a[0]=a[0]*DEG2RAD;
 
-	/* specify output dimensions */
+	/* T0s file: File with ray's traveltime */
 	if(!sf_histint(t0s,"n1",&nt0)) sf_error("No n1= in t0s file");
 	t0 = sf_floatalloc(nt0);
 	sf_floatread(t0,nt0,t0s);
@@ -93,26 +89,7 @@ int main(int argc, char* argv[])
 		slow[im] = 1./(v0*v0);
 	}
 
-	/* initialize ray tracing object */
-	nt = (int) t0[0]/dt0;
-	rt = raytrace_init(2,true,nt,dt0,n,o,d,slow,ORDER);
-	free(slow);
-
-	/* Ray tracing */
-	traj = sf_floatalloc2(ndim,nt+1);
-	
-	/* initialize position */
-	x[0] = s[0][0]; 
-    	x[1] = s[0][1];
-
-	/* initialize direction */
-	p[0] = -cosf(a[0]);
-	p[1] = sinf(a[0]);
-
-	it = trace_ray (rt, x, p, traj);
-	if (it < 0) it = -it; /* keep side-exiting rays */
-
-	sf_putint(rays,"n1",nt);
+	sf_putint(rays,"n1",nr);
 	sf_putint(rays,"n2",1);
 	sf_settype(rays,SF_COMPLEX);
 	sf_putstring(rays,"label1","time");
@@ -122,28 +99,52 @@ int main(int argc, char* argv[])
 	sf_fileflush(rays,NULL);
 	sf_settype(rays,SF_FLOAT);
 
-	/* Write full trajectory */
-	for (i=0; i < nt; i++) {
-		if (0==it || it > i) {
-			sf_floatwrite (traj[i],ndim,rays);
-		} else {
-			sf_floatwrite (traj[it],ndim,rays);
-		}
-    	}
+	if(verb){
+		sf_warning("Input file (Velocity model)");
+		sf_warning("n1=%d d1=%f o1=%f",*n,*d,*o);
+		sf_warning("n2=%d d2=%f o2=%f",*(n+1),*(d+1),*(o+1));
+		sf_warning("Command line parameters");
+		sf_warning("");
+	}
 
-	if(it>0){
-		i = it >= 2 ? it - 2 : it - 1;
-		/* Escape vector */
-		x[0]=traj[it][0];
-		x[1]=traj[it][1];
-		x[0]-=traj[i][0];
-		x[1]-=traj[i][1];
-		/* Dot product with unit vector pointing upward */
-		t = sqrt(x[0]*x[0]+x[1]*x[1]); /* Length */
-		t = acos(x[0]/t);
-		if(x[1]<0) t = -t;
-	}else{
-		t = a[0];
-		t /= DEG2RAD;
+	for(i=0; i<nr; i++){
+
+		/* initialize ray tracing object */
+		nt = (int) t0[0]/dt0;
+		rt = raytrace_init(2,true,nt,dt0,n,o,d,slow,ORDER);
+
+		/* Ray tracing */
+		traj = sf_floatalloc2(ndim,nt+1);
+		
+		/* initialize position */
+		x[0] = s[0][0]; 
+		x[1] = s[0][1];
+
+		/* initialize direction */
+		p[0] = -cosf(a[0]);
+		p[1] = sinf(a[0]);
+
+		it = trace_ray (rt, x, p, traj);
+
+		sf_floatwrite (traj[nt-1],ndim,rays);
+
+		if(it>0){
+			i = it >= 2 ? it - 2 : it - 1;
+			/* Escape vector */
+			x[0]=traj[it][0];
+			x[1]=traj[it][1];
+			x[0]-=traj[i][0];
+			x[1]-=traj[i][1];
+			/* Dot product with unit vector pointing upward */
+			t = sqrt(x[0]*x[0]+x[1]*x[1]); /* Length */
+			t = acos(x[0]/t);
+			if(x[1]<0) t = -t;
+		}else{
+			t = a[0];
+			t /= DEG2RAD;
+		}
+
+		/* Raytrace close */
+		raytrace_close(rt);
 	}
 }

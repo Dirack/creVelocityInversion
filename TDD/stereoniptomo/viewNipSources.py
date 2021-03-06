bdace3ca1a036740953e4cb2fae542a0552703f4
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-03-06 16:27:56 -0300

    Plot NIP sources and m0s #17
    Utility python script to plot the NIP sources using depth coordinates
    and to plot m0s in the acquisition surface. To setup input files run
    'scons -f SConscript2' and after run the viewNipSources.py script. It
    will load generated files and plot sources using matplotlib python
    library

---
+++ TDD/stereoniptomo/viewNipSources.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# viewFoldGraph.py (Python)
+# 
+# Purpose: Plot NIP sources in depth model.
+# 
+# Site: https://dirack.github.io
+# 
+# Version 1.0
+# 
+# Programer: Rodolfo A C Neves (Dirack) 27/09/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+import matplotlib.pyplot as plt
+
+x=[]
+y=[]
+f =  open("nipsources.txt","r")
+for line in f:
+	ys,xs = line.split()
+	x.append(float(xs))
+	y.append(float(ys))
+
+f.close()
+
+z=[]
+f = open("surfsources.txt","r")
+for line in f:
+	z.append(float(line))
+
+f.close()
+
+z0 = [0]*len(z)
+
+plt.xlim(0, 6)
+plt.ylim(3,0)
+plt.plot(list(x),list(y),'*')
+plt.plot(list(z),list(z0),'v')
+plt.show()
